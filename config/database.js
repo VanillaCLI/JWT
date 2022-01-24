7c24f90cb8cc4038da9033393213a8ac26bcd1ba
nayeemjc <nayeem@jayedcorp.com>
2022-01-24 20:30:05 +0600

    Removed Logs

--- config/database.js
+++ config/database.js
@@ -4,9 +4,7 @@ export async function getClient() {
 	let client;
 	try {
 		client = await MongoClient.connect(process.env.CONNECTION_STRING);
-		console.log(client);
 	} catch (err) {
-		console.log(err);
 		return null;
 	}
 
